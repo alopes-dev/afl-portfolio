d0f05b71b1745e7057445e3fb72bc92f415e285d
Antonio Lopes <pdkdidy2011@gmail.com>
2025-09-07 02:31:55 +0100

    style: adjust styling and reorder rankings items
    - Reduce blur intensity for dark mode background
    - Reorder and update active state of rankings items
    - Improve text contrast for active ranking items

--- src/components/AppAbout/_shared/Rankings/Rankings.tsx
+++ src/components/AppAbout/_shared/Rankings/Rankings.tsx
@@ -6,15 +6,22 @@ const rankings = [
     name: "Years of experience",
     active: true,
   },
-  {
-    experience: "+100",
-    name: "Project",
-    active: true,
-  },
+
   {
     experience: "+100",
     name: "Working",
     active: false,
+  },
+
+  {
+    experience: "+100",
+    name: "Working",
+    active: false,
+  },
+  {
+    experience: "+100",
+    name: "Project",
+    active: true,
   },
 ];
 export const Rankings = () => {
@@ -27,14 +34,24 @@ export const Rankings = () => {
             className={cn(
               "relative  p-6 rounded-lg w-[162px] h-[146px] bg-dark-800/30 dark:bg-dark-800 flex flex-col items-center justify-center",
               ranking.active
-                ? "border-[1.36px] border-solid border-purple-dark"
+                ? "border-1 border-solid darK:border-purple-dark bg-purple"
                 : "bg-dark-800/30 dark:bg-dark-800"
             )}
           >
-            <span className="text-5xl font-bold text-center text-light-950 dark:text-dark-100">
+            <span
+              className={cn(
+                "text-5xl font-bold text-center  dark:text-dark-100",
+                ranking.active ? "text-light-100" : "text-light-950"
+              )}
+            >
               {ranking.experience}
             </span>
-            <span className="text-xs font-normal text-purple dark:text-purple-light text-center mt-2">
+            <span
+              className={cn(
+                "text-xs font-normal text-purple dark:text-purple-light text-center mt-2",
+                ranking.active ? "text-light-100" : "text-light-950"
+              )}
+            >
               {ranking.name}
             </span>
           </div>
